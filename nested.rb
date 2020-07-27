ed930b126e849bb6b3daa293b1979ac8c95d47af
Anthony Polito <anthonyalexanderpolito@gmail.com>
2020-07-27 00:24:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,8 +1,3 @@
-describe "nested hash" do
-  describe "hopper" do
-    #it "operates on the programmer_hash and returns the value of the :grace_hopper key" do
-		expect(hopper).to eq({:known_for => "COBOL", :languages => ["COBOL", "FORTRAN"]})
-  end
 
 
 def hopper
